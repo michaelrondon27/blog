c7bd9c77d379255f16613e17dd57b630baa9ce71
Michael <mrondon72@gmail.com>
2018-01-06 19:06:11 -0400

    parte web
    lista, detalle, consulta de categoria y etiquetas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'blog');
+
+Auth::routes();
+
+//web
+Route::get('blog', 'Web\PageController@blog')->name('blog');
+Route::get('entrada/{slug}', 'Web\PageController@post')->name('post');
+Route::get('categoria/{slug}', 'Web\PageController@category')->name('category');
+Route::get('etiqueta/{slug}', 'Web\PageController@tag')->name('tag');
+
+//admin
