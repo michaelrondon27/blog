48b9dc02b87cc6bf2b3b572abf1c5fafcd837f03
Michael <mrondon72@gmail.com>
2018-01-09 13:42:44 -0400

    validar campos obligatorios

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\TagStoreRequest;
+use App\Http\Requests\TagUpdateRequest;
 use App\Http\Controllers\Controller;
 use App\Tag;
 
@@ -40,7 +42,7 @@ class TagController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TagStoreRequest $request)
     {
         $tag = Tag::create($request->all());
         return redirect()->route('tags.edit', $tag->id)->with('info', 'Etiqueta creada con éxito.');
@@ -77,7 +79,7 @@ class TagController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TagUpdateRequest $request, $id)
     {
         $tag = Tag::find($id);
         $tag->fill($request->all())->save();
